0eeff350138a9f77efbf9e9647342e71f28cd008
mkumar0802 <mkumar0802@gmail.com>
2022-04-07 13:16:14 +0530

    Edit Fix

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -88,37 +88,39 @@ export function Edit() {
 
 	return (
 		<>
-		<div className=" bg-slate-200 p-24" >
+			<div className=" bg-slate-200 p-24" >
 
-			<form>
-				<div class="mb-6">
-					<label for="Name" class="block mb-2 text-sm font-medium  ">Name</label>
-					<input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
-				</div>
-				<div class="mb-6">
-					<label for="age" class="block mb-2 text-sm font-medium  ">Employee Id</label>
-					<input type="tel" id="age" value={age} onChange={(e) => setAge(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
-				</div>
-				<div class="mb-6">
-					<label for="country" class="block mb-2 text-sm font-medium  ">country</label>
-					<input type="text" id="country" value={country} onChange={(e) => setCountry(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
-				</div>
+				<form>
+					<h2 onClick={selectUser}   className=" text-xl   md:text-2xl font-bold  "><Link to="/dashboard">Dashboard</Link></h2>
+					<div class="mb-6">
+						<label for="Name" class="block mb-2 text-sm font-medium  ">Name</label>
+						<input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
+					</div>
+					<div class="mb-6">
+						<label for="age" class="block mb-2 text-sm font-medium  ">Employee Id</label>
+						<input type="tel" id="age" value={age} onChange={(e) => setAge(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
+					</div>
+					<div class="mb-6">
+						<label for="country" class="block mb-2 text-sm font-medium  ">country</label>
+						<input type="text" id="country" value={country} onChange={(e) => setCountry(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
+					</div>
 
-				<div class="mb-6">
-					<label for="email" class="block mb-2 text-sm font-medium  ">Your email</label>
-					<input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
-				</div>
+					<div class="mb-6">
+						<label for="email" class="block mb-2 text-sm font-medium  ">Your email</label>
+						<input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
+					</div>
 
-				<div class="mb-6">
-					<label for="mobile" class="block mb-2 text-sm font-medium  ">Your mobile</label>
-					<input type="tel" id="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
-				</div>
+					<div class="mb-6">
+						<label for="mobile" class="block mb-2 text-sm font-medium  ">Your mobile</label>
+						<input type="tel" id="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
+					</div>
 
-				<button onClick={updateUser} type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">  <Link to="/dashboard">Update</Link></button>
-			</form>
+					<button onClick={updateUser} type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">  <Link to="/dashboard">Update</Link></button>
 
-		</div>
-		<Outlet/>
+				</form>
+
+			</div>
+			<Outlet />
 		</>
 	)
 
